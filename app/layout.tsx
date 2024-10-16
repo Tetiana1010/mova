71dac54290bbfc956eec5c09b2604ff4644b698d
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-10-16 18:59:45 +0300

    Implemented favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <ClientProviders>{children}</ClientProviders>
       </body>
